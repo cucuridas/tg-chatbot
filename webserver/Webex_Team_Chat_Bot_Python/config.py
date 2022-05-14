d6c0b64c9c442779b31bad1fc47564f020f5eed6
cucuridas <3310223@naver.com>
2022-05-14 18:47:26 +0900

    Merge branch 'master' of https://github.com/cucuridas/tg-chatbot into TC-20

---
+++ webserver/Webex_Team_Chat_Bot_Python/config.py
@@ -0,0 +1,8 @@
+bot_email = "testcbot@webex.bot"
+bot_name = "testcbot"
+# BOT ACCESS TOKEN
+bearer = "NzdjNzc3MDAtYjM0NC00OWQ5LThiOWQtNzg2MzhiNDM0YThjZjM0ZTFiY2UtMDVm_P0A1_bac71010-7484-48b3-b7ce-a267353178c5"
+# Webhook TargetUrl
+# 도메인 정보는 ngroK에 업로드 될때 변경 될 수 있음
+webhook_url = 'https://dace-2001-2d8-e750-f81-80ab-ccd6-11e2-2b60.jp.ngrok.io'
+webhook_name = 'My_Web_Hook'
